3afc5bb5747701134f25bf015e172a0564e911d9
Brandon Steven Pérez Caamaño <brandons.perez.caamano@gmail.com>
2022-05-17 15:22:23 -0400

    Solucion ejercicio 1

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // 1. Crea una function que tome como parametro un array y devuelva la suma de sus
 // elementos.
+function sumatoria(numeropositivo = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11], numeronegativo = [-1,-3, -2, -6, -7, -9, -8, -4, -5,-12,-11]){  
+    return numeropositivo.length + numeronegativo.length;
+}
+ let resultado = sumatoria()
+ console.log(resultado)
