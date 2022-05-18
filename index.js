c24a297ec66f67d1d921dfed97a3d9b1f28068b6
Brandon Steven Pérez Caamaño <brandons.perez.caamano@gmail.com>
2022-05-18 13:26:44 -0400

    Funcion de Array

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // 1. Crea una function que tome como parametro un array y devuelva la suma de sus
 // elementos.
-function sumatoria(numeropositivo = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11], numeronegativo = [-1,-3, -2, -6, -7, -9, -8, -4, -5,-12,-11]){  
-    return numeropositivo.length + numeronegativo.length;
+
+
+function functionalidad(){
+  let suma = 0;
+  for (let a = 0; a < arguments.length; a++){
+    suma += arguments[a]
+  }
+ return suma;
 }
- let resultado = sumatoria()
- console.log(resultado)
+console.log(functionalidad(2,2,2,2))
